95e778ae6102deeb929114a0a0fdee046e5a61de
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-11-02 15:55:19 +0100

    Updated Map printing

--- src/isen/objectconcept/gamemas/map/Map.java
+++ src/isen/objectconcept/gamemas/map/Map.java
@@ -6,21 +6,32 @@ import isen.objectconcept.gamemas.enums.EntityType;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.function.Consumer;
 
 public class Map {
 
-    private Random random = new Random();
+    private final Random random = new Random();
     // Probability in percentage that a neutral cell contains an obstacle at game initialization
-    private int obstacleProba = 10;
+    private final int obstacleProba = 10;
+    private final int numberCreaturesPerRace = 4;
 
-    private int columns = 10;
-    private int rows = 10;
-    private int safeZoneColumns = 3;
-    private int safeZoneRows = 3;
+    private final int columns = 10;
+    private final int rows = 10;
+    private final int safeZoneColumns = 3;
+    private final int safeZoneRows = 3;
 
     private final MapCell[][] cells = new MapCell[columns][rows];
 
     public Map() {
+        initMap();
+        initCreatures();
+    }
+
+    public MapCell[][] getCells() {
+        return cells;
+    }
+
+    private void initMap() {
         // Create map cells
         for (int y = 0; y < rows; y++) {
             for (int x = 0; x < columns; x++) {
@@ -57,8 +68,42 @@ public class Map {
         }
     }
 
-    public MapCell[][] getCells() {
-        return cells;
+    private void initCreatures() {
+        // init masters
+        cells[0][0].setEntity(new MasterElf());
+        cells[rows - 1][0].setEntity(new MasterGoblin());
+        cells[rows - 1][columns - 1].setEntity(new MasterHuman());
+        cells[0][columns - 1].setEntity(new MasterOrc());
+
+        // init creatures
+        for (int i = 0; i < numberCreaturesPerRace; i++) {
+            pickRandomEmptyCell(0, safeZoneRows, 0, safeZoneColumns)
+                    .setEntity(new Elf("E"+ (i + 1)));
+            pickRandomEmptyCell(0, safeZoneRows, columns - safeZoneColumns, columns)
+                    .setEntity(new Goblin("G"+ (i + 1)));
+            pickRandomEmptyCell(rows - safeZoneRows, rows, columns - safeZoneColumns, columns)
+                    .setEntity(new Human("H"+ (i + 1)));
+            pickRandomEmptyCell(rows - safeZoneRows, rows, 0, safeZoneColumns)
+                    .setEntity(new Orc("O"+ (i + 1)));
+        }
+
+    }
+
+    private MapCell pickRandomEmptyCell(int rowStart, int rowEnd, int columnStart, int columnEnd) {
+        ArrayList<MapCell> emptyCells = new ArrayList<>();
+
+        for (int y = rowStart; y < rowEnd; y++) {
+            for (int x = columnStart; x < columnEnd; x++) {
+                MapCell cell = cells[x][y];
+
+                if (cell.getEntity() == null || cell.getEntity().getType() == EntityType.EMPTY) {
+                    emptyCells.add(cell);
+                }
+            }
+        }
+        MapCell randomCell = emptyCells.get(random.nextInt(emptyCells.size()));
+
+        return cells[randomCell.getX()][randomCell.getY()];
     }
 
     /* ----- MAP PRINT ----- */
@@ -72,23 +117,12 @@ public class Map {
             for (int x = 0; x < columns; x++) {
                 MapCell cell = cells[x][y];
 
-                if (cell.getEntity() == null) {
-                    strMap.append(" |");
-                } else if (cell.getEntity().getType() == EntityType.OBSTACLE) {
-                    strMap.append("x|");
-                } else if (cell.getEntity().getType() == EntityType.ELF) {
-                    strMap.append("E|");
-                } else if (cell.getEntity().getType() == EntityType.GOBLIN) {
-                    strMap.append("G|");
-                } else if (cell.getEntity().getType() == EntityType.HUMAN) {
-                    strMap.append("H|");
-                } else if (cell.getEntity().getType() == EntityType.ORC) {
-                    strMap.append("O|");
-                }
+                strMap.append(" ").append(cell.getEntity().getFigure()).append(" |");
 
                 // if end of map (width), print line
                 if (cell.getX() == columns - 1) {
                     System.out.println(strMap);
+
                     strMap = new StringBuilder("|");
                 }
             }
@@ -107,15 +141,15 @@ public class Map {
                 MapCell cell = cells[x][y];
 
                 switch (cell.getType()) {
-                    case ELF -> strMap.append("E|");
-                    case GOBLIN -> strMap.append("G|");
-                    case HUMAN -> strMap.append("H|");
-                    case ORC -> strMap.append("O|");
+                    case ELF -> strMap.append(" E |");
+                    case GOBLIN -> strMap.append(" G |");
+                    case HUMAN -> strMap.append(" H |");
+                    case ORC -> strMap.append(" O |");
                     case NEUTRAL -> {
-                        if (cell.getEntity().getType() == EntityType.OBSTACLE) {
-                            strMap.append("x|");
+                        if (cell.getEntity() != null && cell.getEntity().getType() == EntityType.OBSTACLE) {
+                            strMap.append(" x |");
                         } else {
-                            strMap.append(" |");
+                            strMap.append("   |");
                         }
                     }
                 }
@@ -123,9 +157,15 @@ public class Map {
                 // if end of map (width), print line
                 if (cell.getX() == columns - 1) {
                     System.out.println(strMap);
+
                     strMap = new StringBuilder("|");
                 }
             }
         }
+    }
+
+    private void printSeparatorLigne() {
+        // add a ligne separator
+        System.out.println("." + "   .".repeat(columns));
     }
 }
