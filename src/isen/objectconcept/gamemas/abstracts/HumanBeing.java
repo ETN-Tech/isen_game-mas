4a4b269875b430d7df53ccefb66147d75fd13f30
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-11-05 19:37:11 +0100

    Added Message management

--- src/isen/objectconcept/gamemas/abstracts/HumanBeing.java
+++ src/isen/objectconcept/gamemas/abstracts/HumanBeing.java
@@ -3,9 +3,12 @@ package isen.objectconcept.gamemas.abstracts;
 import isen.objectconcept.gamemas.enums.Direction;
 import isen.objectconcept.gamemas.enums.EntityType;
 
+import java.util.ArrayList;
+
 public abstract class HumanBeing extends CellEntity {
 
     protected Direction[] availableDirections;
+    protected ArrayList<Message> messages = new ArrayList<>();
 
     protected HumanBeing(EntityType type, String figure, Direction[] availableDirections) {
         super(type, figure);
@@ -14,5 +17,17 @@ public abstract class HumanBeing extends CellEntity {
 
     public Direction[] getAvailableDirections() {
         return availableDirections;
+    }
+
+    /* ----- MESSAGES MANAGEMENT ----- */
+    public void addMessage(Message message) {
+        messages.add(message);
+    }
+
+    public void removeMessage(Message message) {
+        messages.remove(message);
+    }
+    public Message removeMessage(int i) {
+        return messages.remove(i);
     }
 }
--- src/isen/objectconcept/gamemas/abstracts/Message.java
+++ src/isen/objectconcept/gamemas/abstracts/Message.java
@@ -1,4 +1,14 @@
 package isen.objectconcept.gamemas.abstracts;
 
+import isen.objectconcept.gamemas.enums.MessageType;
+
 public abstract class Message {
+
+    protected String message;
+    protected MessageType type;
+
+    public Message(String message, MessageType type) {
+        this.message = message;
+        this.type = type;
+    }
 }
